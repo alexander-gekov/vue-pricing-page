8e8e3018a0838cadf86586d96c8db9b0c11f5f39
Aleksandar Gekov <426537@student.fontys.nl>
2020-04-18 19:33:24 +0300

    added currency and dynamic plans

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" >
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -27,6 +27,6 @@
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #165ba7;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <pricing></pricing>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Pricing from './Pricing'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Pricing
   }
 }
 </script>
