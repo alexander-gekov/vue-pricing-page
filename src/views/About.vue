3e507c22e8095ac733c29a427215c8492ee307ff
Aleksandar Gekov <426537@student.fontys.nl>
2020-04-20 02:54:31 +0300

    fixed margins and padding

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is a pricing page made with Vue and Tailwind.</h1>
+    <h1 class="px-8">This is a pricing page made with Vue and Tailwind.</h1>
   </div>
 </template>
